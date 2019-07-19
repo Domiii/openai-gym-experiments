03228a912ae1ac81dd69b2bcc0351cf04f71cf91
Domiii <dominik.seifert@gmail.com>
2019-07-20 04:24:37 +0800

    - successfully rocked q-learning

--- cart/cart1.py
+++ cart/cart1.py
@@ -27,15 +27,50 @@ stateSpace = (
 actionSpace = (0, 1)
 
 # parameters we can play with
-nIterations = 100  # how long do we want to train for?
-alpha = 0.5
-gamma = 0.98
-stateResolution = 2 # amount of bins for each state variable
+goodConfigs = [
+  {
+    'nIterations': 1000,  # how long do we want to train for?
+    'stateResolution': 4,  # amount of bins for each state variable
+    'alpha': 0.01,
+    'gamma': 0.9
+  },
+  {
+    'nIterations': 100,  # how long do we want to train for?
+    'stateResolution': 6,  # amount of bins for each state variable
+    'alpha': 0.01,
+    'gamma': 0.9
+  },
+  {
+    'nIterations': 200,  # how long do we want to train for?
+    'stateResolution': 6,  # amount of bins for each state variable
+    'alpha': 0.1,
+    'gamma': 0.99
+  },
+  {
+    'nIterations': 100,  # how long do we want to train for?
+    'stateResolution': 10,  # amount of bins for each state variable
+    'alpha': 0.01,
+    'gamma': 0.9
+  }
+]
+configIndex = 2
+
+
+config = goodConfigs[configIndex]
+
+nIterations = config['nIterations']
+stateResolution = config['stateResolution']
+alpha = config['alpha']
+gamma = config['gamma']
+
+nLogSteps = nIterations/20 * 100
+deathValue = -100
+
 
 # epsilon is the probability to favor exploration over exploitation
 # (meaning: take random instead of "good" step)
-def epsilon(iStep, score):
-  return .05 + (1 - (iStep/maxScore)**2) * .5
+def epsilon(iRun):
+  return (1 - (iRun/nIterations))**2
 
 
 
@@ -67,12 +102,16 @@ class Q:
     stateSize = reduce(lambda acc, next: acc*next, (len(s) for s in stateSpace))
     actionSize = len(actionSpace)
     qLen = stateSize * actionSize
-    self.q = np.zeros(qLen) * initialQ
+    self.q = np.zeros(qLen) + initialQ
+
+  def hasFinished(self):
+    return self.nSteps >= maxScore
 
   def reset(self):
     observation = self.env.reset()
     iState = self.lastStateIndex = self.getStateIndex(observation)
     self.nextAction = self.getMaxAction(iState)
+    self.nSteps = 0
 
   # convert observation data into a single number representing the 1D index of the state-value space
   def getStateIndex(self, observation):
@@ -124,19 +163,21 @@ class Q:
       elif thisQ == maxQ:
         nMax += 1
         maxActions[nMax-1] = i
-        maxQ = thisQ
     return maxActions, nMax
 
 
   def stepRandom(self):
     # pick an action at random
     action = np.random.choice(self.actionSpace)
-    return self.stepAction(action)
+    results = self.stepAction(action)
+    return results
     
 
   def step(self):
+    action = self.getMaxAction(self.lastStateIndex)
     # go with the action we already determined to be optimal last step
-    return self.stepAction(self.nextAction)
+    # action = self.nextAction
+    return self.stepAction(action)
 
 
   def stepAction(self, action):
@@ -145,21 +186,31 @@ class Q:
     # take step
     observation, reward, done, info = self.env.step(action)
     nextIState = self.lastStateIndex = self.getStateIndex(observation)
-    nextAction = self.nextAction = self.getMaxAction(nextIState)
+    maxNextAction = self.nextAction = self.getMaxAction(nextIState)
+    self.nSteps += 1
 
-    # the max value to be expected from here on forward
-    nextMax = q[nextIState + nextAction]
-    newVal = reward + gamma * nextMax
+    # death should be punished hard
+    died = done and self.nSteps < maxScore
+    if died:
+      q[iState + action] = reward = deathValue
+    else:
+      # the max value to be expected from here on forward
+      nextMax = q[nextIState + maxNextAction]
+      newVal = reward + gamma * nextMax
 
-    # update Q value of new state
-    q[iState + action] = (1-alpha) * q[iState + action] + (alpha) * newVal
+      # update Q value of new state
+      oldQ = q[iState + action]
 
-    return reward, done, info
+      #print(f'{iState+action} -> {nextIState + nextAction}')
+
+      q[iState + action] = (1-alpha) * oldQ + (alpha) * newVal
+
+    return reward, done
 
   # Make sure, some basic things are still in order
   # E.g.: all numbers are in sane range.
   def doSanityChecks(self):
-    assert all(x >= 0 for x in self.q), "Q state-action value function has insane values"
+    assert all(x >= deathValue for x in self.q), "Q state-action value function has insane values"
   
   def getMetaData(self):
     return self.q
@@ -170,91 +221,118 @@ class Q:
 # Run simulation with current Q (state-action value function)
 ##########################################################################################
 
-def runOnce(q, epsilon):
+def runOnce(q, eps):
   q.reset()
   done = False
   iStep = 0
   nRandom = 0
   score = 0
-  startTime = perf_counter()
 
   # run simulation for this trial (max 200 steps)
   while not done:
     iStep += 1
 
-    if np.random.uniform() < epsilon(iStep, score):
+    if np.random.uniform() < eps:
       nRandom += 1
-      reward, done, _ = q.stepRandom()
+      reward, done = q.stepRandom()
     else:
-      reward, done, _ = q.step()
+      reward, done = q.step()
 
     score += reward
 
-  elapsed = perf_counter() - startTime
+  return score, nRandom
 
-  return score, elapsed, nRandom
 
+'''
+TODO:
+1. multithreading and cleaner code for plotting and other debugging stuff
+2. add semi-automated parameter tuning
+3. improve epsilon:
+  -> maybe for later? actually do research on better epsilon functions!
+  -> consider current maturity of trained results instead of nIterations?
+  -> restart if not found within given amount of rounds?
+'''
+
+env = gym.make(envName)
+q = Q(env, alpha, gamma, stateSpace, actionSpace)
+
+# setup plot
+fig, axs = plt.subplots(2, 1, constrained_layout=True)
+
+# draw it once
+# animated scatter plot link: https://stackoverflow.com/questions/43674917/animation-in-matplotlib-with-scatter-and-using-set-offsets-autoscale-of-figure
+ys = q.q
+xs = np.array(range(len(ys)))
+qShape = axs[0].scatter(xs, ys, s=1)
+lines = [
+    axs[1].plot([1], [0])[0],
+    axs[1].plot([1], [0])[0]
+]
+axs[1].set_ylim([0, maxScore])
+scores = []
+randoms = []
+times = []
+plt.ion()
+plt.show(block=False)
+
+def drawResults(i):
+  #print(f'runOnce, score: {score}, rand: {nRandom/q.nSteps}')
+  # re-draw it!
+  Qs = np.c_[xs, q.q]
+  # xs = 0.9 * np.random.rand(512)
+  # ys = 0.9 * np.random.rand(512)
+  # Qs = np.c_[xs, ys]
+  qShape.set_offsets(Qs)
+
+  # relim does not work for scatter; see: https://stackoverflow.com/a/51327480
+  axs[0].ignore_existing_data_limits = True
+  axs[0].update_datalim(qShape.get_datalim(axs[0].transData))
+  axs[0].autoscale_view()
+
+  axs[1].set_xlim([0, i+1])
+  lines[0].set_data(times, scores)
+  lines[1].set_data(times, randoms)
+
+  fig.canvas.draw()
+  fig.canvas.flush_events()
+  plt.pause(0.0001)
 
 # run!
-def runN(n):
-  env = gym.make(envName)
-  q = Q(env, alpha, gamma, stateSpace, actionSpace)
-
-  # setup plot
-  #plt.ion()
-  fig, axs = plt.subplots(2, 1, constrained_layout=True)
-
-  # draw it once
-  # animated scatter plot link: https://stackoverflow.com/questions/43674917/animation-in-matplotlib-with-scatter-and-using-set-offsets-autoscale-of-figure
-  ys = q.q
-  xs = np.array(range(len(ys)))
-  qShape = axs[0].scatter(xs, ys, s=5)
-  scoreShape, = axs[1].plot([1], [0])
-  axs[1].set_ylim([0, maxScore])
-  scores = []
-  scoreXs = []
-  plt.ion()
-  plt.show(block=False)
-
+def runN(q, n):
+  totalElapsed = 0
+  startTime = perf_counter()
   for i in range(n):
-    score, elapsed, nRandom = runOnce(q, epsilon)
-    scores.append(score)
-    scoreXs.append(i)
-
-    #print(f'runOnce, score: {score}, elapsed: {elapsed}, rand: {nRandom/score}, maxQ: {max(q.q)}')
-
-    if not (i % 1000):
-      # re-draw it!
-      Qs = np.c_[xs, q.q]
-      # xs = 0.9 * np.random.rand(512)
-      # ys = 0.9 * np.random.rand(512)
-      # Qs = np.c_[xs, ys]
-      qShape.set_offsets(Qs)
-
-      # relim does not work for scatter; see: https://stackoverflow.com/a/51327480
-      axs[0].ignore_existing_data_limits = True
-      axs[0].update_datalim(qShape.get_datalim(axs[0].transData))
-      axs[0].autoscale_view()
-
-      axs[1].set_xlim([0, i+1])
-      scoreShape.set_data(scoreXs, scores)
-
-      fig.canvas.draw()
-      fig.canvas.flush_events()
-      plt.pause(0.0001)
+    eps = epsilon(i)
+    score, nRandom = runOnce(q, eps)
+    # scores.append(q.nSteps)
+    # randoms.append(nRandom)
+    # times.append(i)
 
     # make sure, shit's still ok
     q.doSanityChecks()
 
-    # done!
-    if score >= maxScore:
-      print('GOOD!')
+    # show some stuff
+    if i % nLogSteps == 0:
+      drawResults(i)
+      pass
+
+    if q.hasFinished():
+        # done!
+      drawResults(i)
       break
 
-  plt.pause(2)
+  elapsed = perf_counter() - startTime
+  print(f'finished episode, took {elapsed}s')
+  return q.hasFinished()
+  #plt.pause(3)
 
 
-runN(5000)
+
+while not runN(q, nIterations): pass
+
+print('Done!')
+
+plt.pause(3)
 
 #input('Press ENTER to exit...')
 
